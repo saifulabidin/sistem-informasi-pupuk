4e9a2383fac455d9f9bd8bce87b105b15c0c6149
saifulabidin <syaiful.osd@yahoo.com>
2025-04-27 13:47:38 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +46,19 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    
+    /**
+     * Determine if the user can access the Filament admin panel.
+     *
+     * @param Panel $panel
+     * @return bool
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // Allow all authenticated users to access the panel
+        
+        // For production, you might want to implement proper authorization:
+        // return str_ends_with($this->email, '@yourdomain.com') || $this->hasRole('admin');
     }
 }
